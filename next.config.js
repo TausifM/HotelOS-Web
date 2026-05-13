0c11d4f94a1e797716fefb6babaea060fc0678c7
TausifM <heytausifsheikh@gmail.com>
2026-05-13 21:32:18 +0530

    eslint removed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  // REMOVE OR COMMENT OUT THIS LINE:
+  // output: 'standalone', 
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '*.amazonaws.com' },
